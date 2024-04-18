80a1684c6b58f5007ba269d157181fca67f4cd03
cfstout <clayton.stout@gemini.com>
2024-04-18 10:06:40 -0600

    Add an endpoint to get by email

--- src/main/kotlin/io/github/cfstout/keycloak/endpoints/UserEndpoints.kt
+++ src/main/kotlin/io/github/cfstout/keycloak/endpoints/UserEndpoints.kt
@@ -3,6 +3,8 @@ package io.github.cfstout.keycloak.endpoints
 import io.github.cfstout.keycloak.dao.DaoFactory
 import io.github.cfstout.keycloak.dao.DaoHelpers.txnWithDao
 import io.github.cfstout.keycloak.endpoints.models.CreateUserRequest
+import io.github.cfstout.keycloak.models.Email
+import io.github.cfstout.keycloak.models.users.User
 import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.features.NotFoundException
@@ -40,6 +42,18 @@ class UserEndpoints(app: Application, jooq: DSLContext, private val daoFactory: 
                             dao.getUserById(id)
                         }
                     call.respond(user ?: throw NotFoundException("User $id not found"))
+                }
+                get("/email/{email}") {
+                    val email: Email =
+                        Email.from(
+                            call.parameters["email"]
+                                ?: throw IllegalArgumentException("No email provided"),
+                        ) ?: throw IllegalStateException("Email is invalid")
+                    val user: User? =
+                        jooq.txnWithDao(daoFactory::userDao) { dao ->
+                            dao.getUserByEmail(email)
+                        }
+                    call.respond(user ?: throw NotFoundException("User $email not found"))
                 }
             }
         }
